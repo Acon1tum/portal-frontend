e2a694052d4315a5c294fbda02eebf35bac4b10c
CHNCHN5 <christianasejo5@gmail.com>
2025-08-11 10:22:04 +0800

    push ulit

--- src/components/custom-ui/auth/DesignLeft.tsx
+++ src/components/custom-ui/auth/DesignLeft.tsx
@@ -17,7 +17,7 @@ const DesignLogin = ({ animation = "submarine" }: DesignLoginProps) => {
     <div className="hidden md:block w-full h-full">
       <div className="w-full h-full relative overflow-hidden">
         {theme === "dark" && (
-          <div className="absolute inset-0" />
+          <div className="absolute inset-0 bg-blue-600/0 z-10" />
         )}
         <div className="absolute bottom-8 left-8 flex flex-col gap-2 z-20">
           <h1 className="text-white text-5xl font-bold drop-shadow-[0_2px_2px_rgba(0,0,0,1)]">Marino Portal</h1>
